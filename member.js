ca944d67c220d92687f9b9104b0505e8aeebe8c9
demirEbru <151520999+demirEbru@users.noreply.github.com>
2023-11-24 07:04:56 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,56 @@
+function skillsMember() {
+    // Path: member.js
+    var member = {
+        id: 1,
+        name: 'John Doe',
+        skills: ['JavaScript', 'CSS', 'HTML']
+    };
+    // Path: member.js
+    var member = {
+        id: 1,
+        name: 'John Doe',
+        skills: ['JavaScript', 'CSS', 'HTML']
+    };
+    // Path: member.js
+    var member = {
+        id: 1,
+        name: 'John Doe',
+        skills: ['JavaScript', 'CSS', 'HTML']
+    };
+    // Path: member.js
+    var member = {
+        id: 1,
+        name: 'John Doe',
+        skills: ['JavaScript', 'CSS', 'HTML']
+    };
+    // Path: member.js
+    var member = {
+        id: 1,
+        name: 'John Doe',
+        skills: ['JavaScript', 'CSS', 'HTML']
+    };
+    // Path: member.js
+    var member = {
+        id: 1,
+        name: 'John Doe',
+        skills: ['JavaScript', 'CSS', 'HTML']
+    };
+    // Path: member.js
+    var member = {
+        id: 1,
+        name: 'John Doe',
+        skills: ['JavaScript', 'CSS', 'HTML']
+    };
+    // Path: member.js
+    var member = {
+        id: 1,
+        name: 'John Doe',
+        skills: ['JavaScript', 'CSS', 'HTML']
+    };
+    // Path: member.js
+    var member = {
+        id: 1,
+        name: 'John Doe',
+        skills: ['JavaScript', 'CSS', 'HTML']
+    };
+}
